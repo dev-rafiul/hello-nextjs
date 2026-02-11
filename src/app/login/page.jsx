043471e4544b65b52790eff984ebbbef13402608
COMRADE040 <rafiulislam040@gmail.com>
2026-02-11 23:15:32 +0600

    first commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Title>Welcome To Login Page</Title>
+
+
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Title>Welcome To Register</Title>
+        </div>
+    );
+};
+
+export default page;
